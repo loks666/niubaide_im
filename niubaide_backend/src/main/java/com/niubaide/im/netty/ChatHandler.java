b49e97342124d4d5547928fcd7f1d534aef51e20
李翔 <super_xiang@126.com>
2020-06-13 14:51:14 +0800

    连不成功

---
+++ niubaide_backend/src/main/java/com/niubaide/im/netty/ChatHandler.java
@@ -0,0 +1,46 @@
+package com.niubaide.im.netty;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * 处理消息的handler
+ * TextWebSocketFrame: 在netty中，是用于为websocket专门处理文本的对象，frame是消息的载体
+ */
+public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    // 用来保存所有的客户端连接
+    private static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:MM");
+
+    // 当Channel中有新的事件消息会自动调用
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        // 当接收到数据后会自动调用
+
+        // 获取客户端发送过来的文本消息
+        String text = msg.text();
+        System.out.println("接收到消息数据为：" + text);
+
+        for (Channel client : clients) {
+            // 将消息发送到所有的客户端
+            client.writeAndFlush(new TextWebSocketFrame(sdf.format(new Date()) + ":" + text));
+        }
+    }
+
+    // 当有新的客户端连接服务器之后，会自动调用这个方法
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        // 将新的通道加入到clients
+        clients.add(ctx.channel());
+    }
+}
---
+++ niubaide_backend/src/main/java/com/niubaide/im/netty/WebSocketServer.java
@@ -0,0 +1,35 @@
+package com.niubaide.im.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebSocketServer {
+
+    private EventLoopGroup bossGroup;       // 主线程池
+    private EventLoopGroup workerGroup;     // 工作线程池
+    private ServerBootstrap server;         // 服务器
+    private ChannelFuture future;           // 回调
+    @Value("${websocket.port}")
+    private int port;
+
+    public void start() {
+        future = server.bind(port);
+        System.err.println("netty server - " + port + " 启动成功");
+    }
+
+    public WebSocketServer() {
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup();
+
+        server = new ServerBootstrap();
+        server.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new WebsocketInitializer());
+    }
+}
