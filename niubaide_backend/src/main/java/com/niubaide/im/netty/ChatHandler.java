22064fe1b2f90ee4f54aea283ef2980c0e9c3d89
jax <284190056@qq.com>
2020-11-27 23:53:11 +0800

    修改下代码规范

--- niubaide_backend/src/main/java/com/niubaide/im/netty/ChatHandler.java
+++ niubaide_backend/src/main/java/com/niubaide/im/netty/ChatHandler.java
@@ -7,23 +7,26 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import org.apache.commons.lang3.StringUtils;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
  * 处理消息的handler
  * TextWebSocketFrame: 在netty中，是用于为websocket专门处理文本的对象，frame是消息的载体
+ * @author 李翔
  */
 public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
-    // 用来保存所有的客户端连接
+    /**
+     * 用来保存所有的客户端连接
+     */
     private static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:MM");
 
-    // 当Channel中有新的事件消息会自动调用
+    /**
+     * 当Channel中有新的事件消息会自动调用
+     */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
         // 当接收到数据后会自动调用
@@ -38,7 +41,11 @@ public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame>
         }
     }
 
-    // 当有新的客户端连接服务器之后，会自动调用这个方法
+    /**
+     * 当有新的客户端连接服务器之后，会自动调用这个方法
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         // 将新的通道加入到clients
