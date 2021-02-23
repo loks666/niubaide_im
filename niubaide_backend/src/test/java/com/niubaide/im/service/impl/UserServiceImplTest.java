80b6ef38c2fea8b578b608a5e1477bf585899577
jaxs <284190056@qq.com>
2021-02-23 19:15:34 +0800

    数据库加密失败了，下次重新试下

--- niubaide_backend/src/test/java/com/niubaide/im/service/impl/UserServiceImplTest.java
+++ niubaide_backend/src/test/java/com/niubaide/im/service/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.niubaide.im.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.niubaide.im.mapper.UserMapper;
 import com.niubaide.im.pojo.po.TbUser;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
@@ -12,17 +13,22 @@ import org.springframework.util.DigestUtils;
  * @Description
  * @Date 2020/6/14 18:04
  */
-class UserServiceImplTest {
+public class UserServiceImplTest {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    StringEncryptor encryptor;
+
     @Test
-    void getAllUser() {
+    public void getAllUser() {
+        String test = encryptor.decrypt("test");
+        System.out.println(test);
     }
 
 
     @Test
-    void login() {
+    public void login() {
         TbUser tbUser = new TbUser();
         tbUser.setUsername("xiaoming");
         tbUser.setPassword("c4ca4238a0b923820dcc509a6f75849b");
---
+++ niubaide_backend/src/test/java/com/niubaide/im/webapp/PwdTest.java
@@ -0,0 +1,26 @@
+package com.niubaide.im.webapp;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+
+public class PwdTest {
+
+    @Autowired
+    @Qualifier("encryptor")
+    private StringEncryptor encryptor;
+
+    @Test
+    public void getPass() {
+        String url = encryptor.encrypt("jdbc:mysql://81.68.219.235:3306/hchat?useUnicode=true&characterEncoding=utf8");
+        String name = encryptor.encrypt("root");
+        String password = encryptor.encrypt("mysql");
+        System.out.println(url);
+        System.out.println(name+"----------------");
+        System.out.println(password+"----------------");
+//        Assert.assertTrue(name.length() > 0);
+//        Assert.assertTrue(password.length() > 0);
+    }
+}
