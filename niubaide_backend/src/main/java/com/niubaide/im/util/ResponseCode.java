f38f60abcdbce7286b97fcf9bd90654293a1c989
jaxs <284190056@qq.com>
2021-02-25 17:15:41 +0800

    改个bug

--- niubaide_backend/src/main/java/com/niubaide/im/util/ResponseCode.java
+++ niubaide_backend/src/main/java/com/niubaide/im/util/ResponseCode.java
@@ -6,7 +6,7 @@ public enum ResponseCode {
     LOGIN_SUCCESS(201, "登录成功！"),
     REGISTER_SUCCESS(202, "注册成功！"),
     ERROR(300, "错误"),
-    NULL_PARAM(301, "参数不能为空！"),
+    NULL_PARAM(301, "用户名或密码错误！"),
     ILLEGAL_PARAMETER(302, "非法参数！"),
     REGISTER_FAIL(303, "注册失败！"),
     LOGIN_FAILED(303, "登录失败！请检查用户名和密码是否正确！"),
